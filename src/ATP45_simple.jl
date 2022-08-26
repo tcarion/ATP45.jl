8732e65d444868f590352c008d77d2be60bcc956
tcarion <tristancarion@gmail.com>
2022-08-26 08:39:31 +0000

    more detailed atp cases

--- src/ATP45_simple.jl
+++ src/ATP45_simple.jl
@@ -1,39 +1,48 @@
 const CONTAINERS = Dict(
     :BML => Dict(
         :name => "Bomblet",
-        :type => 1
+        :type => 1,
+        :stab => 10
     ),
     :SHL => Dict(
         :name => "Shell",
-        :type => 1
+        :type => 1,
+        :stab => 10
     ),
     :MNE => Dict(
         :name => "Mine",
-        :type => 1
+        :type => 1,
+        :stab => 10
     ),
     :SB_RKT => Dict(
         :name => "Surface Burst Rocket",
-        :type => 1
+        :type => 1,
+        :stab => 15
     ),
     :SB_MSL => Dict(
         :name => "Surface Burst Missile",
-        :type => 1
+        :type => 1,
+        :stab => 15
     ),
     :BOM => Dict(
         :name => "Bomb",
-        :type => 2
+        :type => 2,
+        :stab => 15
     ),
     :NKN => Dict(
         :name => "Unknown",
-        :type => 2
+        :type => 2,
+        :stab => 15
     ),
     :AB_RKT => Dict(
         :name => "Air Burst Rocket",
-        :type => 2
+        :type => 2,
+        :stab => 15
     ),
     :AB_MSL => Dict(
         :name => "Air Burst Missile",
-        :type => 2
+        :type => 2,
+        :stab => 15
     ),
     :SPR => Dict(
         :name => "Tank",
@@ -58,8 +67,37 @@ const PROCEDURES = Dict(
     ),
     :C => Dict(
         :name => "Unobserved Release"
+    ),
+    :P => Dict(
+        :name => "Localized Point Release"
+    ),
+    :Q => Dict(
+        :name => "Large Area Release"
+    ),
+    :R => Dict(
+        :name => "Unknown Container"
     )
 )
+
+
+const INCIDENTS = Dict(
+    :chem => Dict(
+        :name => "Chemical"
+    ),
+    :bio => Dict(
+        :name => "Biological"
+    ),
+    :radio => Dict(
+        :name => "Radiological"
+    ),
+    :nucl => Dict(
+        :name => "Nuclear"
+    )
+)
+
+
+@enum StabilityClass U N S
+
 
 abstract type AbstractWind end
 mutable struct WindCoords <: AbstractWind
@@ -81,6 +119,8 @@ mutable struct Atp45Input
     container::Symbol
     "Type of procedure"
     procedure::Symbol
+    "Stability Class"
+    stab::StabilityClass
 end
 
 mutable struct Atp45Result
@@ -90,32 +130,89 @@ mutable struct Atp45Result
     input::Atp45Input
 end
 
-function run(input::Atp45Input)
+
+
+"""
+    run_chem(input)
+
+Calculate the release and hazard area according to the case we want, for a chemical incident (Page 3-1)
+"""
+function run_chem(input::Atp45Input)
     #result::FeatureCollection = foo()
-    if input.procedure == :A
-        Atp45Result(proc(input, 1000.), input)
-    elseif input.procedure == :B
+    if input.procedure == :A                     # Page 3-15 and 3-16, Type A, Cases 1 and 2
+        if input.stab == U
+            if CONTAINERS[input.container][:stab] == 10                     
+                Atp45Result(proc(input, 10000., 1000., 10000.), input)
+            elseif CONTAINERS[input.container][:stab] == 15
+                Atp45Result(proc(input, 15000., 1000., 10000.), input)
+            end
+        elseif input.stab == N
+            Atp45Result(proc(input, 30000., 1000., 10000.), input)
+        elseif input.stab == S
+            Atp45Result(proc(input, 50000., 1000., 10000.), input)
+        end
+    elseif input.procedure == :B                 # Page 3-18 to 3-25, Type B, all cases
         Atp45Result(typeB(input), input)
+    elseif input.procedure == :C                 # Page 3-26, Type C, Cases 1 and 2
+        Atp45Result(Unobserved(input, 10000.), input)
+    elseif input.procedure == :simplified        # Page 3-7 to 3-9, Simplified Procedure, Cases 1 and 2
+        Atp45Result(proc(input, 10000., 2000., 10000.), input)
+    else
+        error("The procedure type $(input.procedure) is not recognized")
+    end
+end
+# There is a type D not implemented yet because it depends on the chemical substance released
+#All infos Page 3-34
+
+
+
+"""
+    run_bio(input)
+
+Calculate the release and hazard area according to the case we want, for a biological incident (Page 4-1)
+"""
+function run_bio(input::Atp45Input)
+    if input.procedure == :simplified
+        Atp45Result(proc(input, 10000., 2000., 10000.), input)
+    elseif input.procedure == :P
+        Atp45Result(proc(input, 10000., 2000., 20000.), input)
+    elseif input.procedure == :Q
+        Atp45Result(proc(input, 10000., 10000., 20000.), input)
+    elseif input.procedure == :R
+        Atp45Result(typeB_2(input, 2000., res), input)
     elseif input.procedure == :C
-        Atp45Result(typeC(input), input)
-    elseif input.procedure == :simplified
-        Atp45Result(proc(input, 2000.), input)
+        Atp45Result(Unobserved(input, 50000.), input)
     else
         error("The procedure type $(input.procedure) is not recognized")
     end
 end
 
+
+
+#TODO: implementation of radiological and nuclear incident with run_radio and run_nucl (Page 5-1 and 6-1)
+
+
+
+"""
+    windCoords(wind)
+
+Convert the speed and azimuth of the wind into the coordinates of the wind vector.
+"""
 function windCoords(wind::WindAzimuth)
     u = wind.speed*cosd(90 - wind.azimuth)
     v = wind.speed*sind(90 - wind.azimuth)
     return u, v
 end
 
+
+
 """
+    wind_speed(Vx, Vy)
+
 Vx and Vy are the components of the wind speed vector on the West-East and South-North directions respectively
+Return the wind speed
 
 """
-
 function wind_speed(Vx, Vy)
     return sqrt(Vx^2 + Vy^2)
 end
@@ -127,13 +224,12 @@ function wind_speed(wind::WindAzimuth)
 end
 
 
+
 """
     wind_direction(Vx, Vy)
 
 Return the angle between the wind vector and the West-East direction
-
 """
-
 function wind_direction(Vx, Vy)
     if Vx >= 0
         return atand(Vy/Vx)
@@ -143,11 +239,12 @@ function wind_direction(Vx, Vy)
 end
 
 
+
 """
+    azimuth(Vx, Vy)
+
 Azimuth in degrees, the reference direction is North
-
 """
-
 function azimuth(Vx, Vy)
     return (90. - wind_direction(Vx, Vy))
 end
@@ -159,13 +256,12 @@ function azimuth(lon1, lat1, lon2, lat2)
 end
 
 
+
 """
     circle_area(lon::AbstractFloat, lat::AbstractFloat, radius::AbstractFloat, res)
 
 Calculate the coordinates of the release or the hazard area (circle)
-
 """
-
 function circle_area(lon::AbstractFloat, lat::AbstractFloat, radius::AbstractFloat, res = 360)
     azimuth = range(0., 360., length = res)
     coords = []
@@ -176,13 +272,12 @@ function circle_area(lon::AbstractFloat, lat::AbstractFloat, radius::AbstractFlo
 end
 
 
+
 """
     hazard_area_triangle(lon, lat, Vx, Vy, hauteur, radius)
 
 Calculate the coordinates of the ends of the hazard area (a triangle) in the case of a wind speed > 10km/h
-
 """
-
 function hazard_area_triangle(lon, lat, Vx, Vy, dist, radius)
     l = dist/cosd(30)
     coords = []
@@ -200,14 +295,19 @@ end
 
 
 
-function typeB_2(input::Atp45Input, res = 360)
+"""
+Page 3-24 and 3-25 in NATO ATP-45: Warning  and Reporting and Hazard Prediction of CBRN Incidents (Operators Manual)
+
+Type B, Case 5/6 of the detailled procedures
+"""
+function typeB_2(input::Atp45Input, radius, res = 360)
     lon1 = input.locations[1][1]
     lat1 = input.locations[1][2]
     lon2 = input.locations[2][1]
     lat2 = input.locations[2][2]
 
-    release_area1 = circle_area(lon1, lat1, 1000., res)
-    release_area2 = circle_area(lon2, lat2, 1000., res)
+    release_area1 = circle_area(lon1, lat1, radius, res)
+    release_area2 = circle_area(lon2, lat2, radius, res)
 
     az = Int(round(azimuth(lon1, lat1, lon2, lat2)))
     if az < 0
@@ -309,7 +409,6 @@ function typeB_2(input::Atp45Input, res = 360)
                 coords = [vcat([hazard_triangle1[2]], [hazard_triangle1[3]], [hazard_triangle2[3]], [hazard_triangle2[1]], [hazard_triangle2[2]])]
             end
         end
-
         
         hazard_area = Polygon(coords)
         prop2 = Dict("type" => "hazard", "shape" => "triangles")
@@ -320,19 +419,23 @@ end
 
 
 
+"""
+    proc(input, radius_release, radius_hazard, res = 360)
 
-function proc(input::Atp45Input, radius, res = 360)
+Calculate the release and hazard area according to the radius we want. Useful for simplified procedure and type A/B.
+"""
+function proc(input::Atp45Input, dist, radius_release, radius_hazard, res = 360)
     lon = input.locations[1][1]
     lat = input.locations[1][2]
-    release_area = Polygon([circle_area(lon, lat, radius, res)])
+    release_area = Polygon([circle_area(lon, lat, radius_release, res)])
     prop1 = Dict("type" => "release", "shape" => "circle")
     features = [Feature(release_area, prop1)]
     if wind_speed(input.wind) <= 10
-        hazard_area = Polygon([circle_area(lon, lat, 10000., res)])
+        hazard_area = Polygon([circle_area(lon, lat, radius_hazard, res)])
         prop2 = Dict("type" => "hazard", "shape" => "circle")
         push!(features, Feature(hazard_area, prop2))
     else
-        hazard_area = Polygon([hazard_area_triangle(lon, lat, input.wind, 10000. + 2*radius, radius)])
+        hazard_area = Polygon([hazard_area_triangle(lon, lat, input.wind, dist + 2*radius_release, radius_release)])
         prop2 = Dict("type" => "hazard", "shape" => "triangle")
         push!(features, Feature(hazard_area, prop2))
     end
@@ -341,21 +444,33 @@ end
 
 
 
+"""
+Page 3-18 to 3-25 in NATO ATP-45: Warning  and Reporting and Hazard Prediction of CBRN Incidents (Operators Manual)
+
+All Type B cases
+"""
 function typeB(input::Atp45Input, res = 360)
-    if CONTAINERS[input.container][:type] == 1
-        proc(input, 1000., res)
-    elseif CONTAINERS[input.container][:type] == 2
-        proc(input, 2000., res)
-    elseif CONTAINERS[input.container][:type] == 3
-        typeB_2(input, res)
+    if CONTAINERS[input.container][:type] == 1   # Page 3-19 and 3-20, Type B, Cases 1 and 2
+        proc(input, 10000., 1000., 10000., res)
+    elseif CONTAINERS[input.container][:type] == 2    # Page 3-21 to 3-23, Type B, Cases 3 and 4
+        proc(input, 10000., 2000., 10000., res)
+    elseif CONTAINERS[input.container][:type] == 3    # Page 3-24 and 3-25, Type B, Cases 5 and 6
+        typeB_2(input, 1000., res)
     end
 end
 
 
-function typeC(input::Atp45Input, res = 360)
+
+
+"""
+Page 3-26 in NATO ATP-45: Warning  and Reporting and Hazard Prediction of CBRN Incidents (Operators Manual)
+
+Type C/S
+"""
+function Unobserved(input::Atp45Input, radius, res = 360)
     lon = input.locations[1][1]
     lat = input.locations[1][2]
-    hazard_area = Polygon([circle_area(lon, lat, 10000., res)])
+    hazard_area = Polygon([circle_area(lon, lat, radius, res)])
     prop = Dict("type" => "hazard", "shape" => "circle")
     feature = [Feature(hazard_area, prop)]
     FeatureCollection(feature)
--- test/atp45_simple.jl
+++ test/atp45_simple.jl
@@ -1,10 +1,20 @@
 using Test
 using ATP45
 
-input = Atp45Input([[50., 3.]], WindCoords(10., 10.), :BOM, "TypeB")
-ATP45.run(input)
-
 lon, lat = 2.5, 46.
+lon1, lat1 = 2.6, 45.7
 Vx, Vy = 0., 10.
 @test ATP45.hazard_area_triangle(lon, lat, Vx, Vy, 14000., 2000.)[1][1] == lon
 @test ATP45.hazard_area_triangle(lon, lat, Vy, Vx, 14000., 2000.)[1][2] ≈ lat atol=1e-4
+@test ATP45.circle_area(lon, lat, 1000., 360)[1][1] == lon
+
+input = Atp45Input([[lon, lat], [lon1, lat1]], WindAzimuth(15., 0.), :SPR, :B, U)
+a = ATP45.run(input)
+coords = a.collection.features[2].geometry.coordinates[1]
+@test coords[1][1] == lon
+@test coords[5][1] == lon1
+
+input1 = Atp45Input([[lon, lat]], WindAzimuth(15., 90.), :BOM, :A, U)
+b = ATP45.run(input1)
+coords1 = b.collection.features[2].geometry.coordinates[1]
+@test coords1[1][2] ≈ lat atol=1e-4
