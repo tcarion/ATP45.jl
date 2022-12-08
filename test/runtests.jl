5217c2c8f2ce817904ffee661798c9a35bd2a74b
Tristan Carion <tristancarion@gmail.com>
2022-12-08 07:17:29 +0100

    move to proj, wrong results

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,9 +4,10 @@ using Proj4
 
 @testset "ATP45.jl" begin
     @testset "Horizontal walk with azimuth and distance" begin
-        lon, lat = 4., 0.
-        dist = 111321.
-        dest = ATP45.horizontal_walk(lon, lat, dist, 90.)
+        lon, lat = 4., 50.
+        distance = 111321.
+        azimuth = 90.
+        dest = ATP45.horizontal_walk(lon, lat, dist)
         @test dest ≈ [lon+1, lat] atol=1e-4
    end
 
